ea5607e08ac6623a0cb2ea268e682e4ef357564c
Nathan Claudel <nathan.claudel@telecom-paristech.fr>
2018-12-07 10:00:45 +0100

    added code

---
+++ mips_program.c
@@ -0,0 +1,18 @@
+int __attribute__((naked)) f(char *str)
+{
+    if(str == 0) return -1;
+    
+    char a1 = *str;
+
+    int cpt = 0;
+
+    while(a1 != 0)
+    {
+        if(a1 != 0x20) cpt++;
+
+        str ++; 
+        a1 = *str;
+    }
+
+    return cpt;
+}
